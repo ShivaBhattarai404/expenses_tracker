ShivaBhattarai404/test

Add date, multi expenses addtion feature with fileSystem as database

backend/Model/Expenses.js
@@ -1,15 +1,158 @@
-const {default: mongoose, Schema} = require("mongoose");
+const path = require("path");
 
-const expenseSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    amount: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-}, {timestamps: true})
+const { readFile, writeFile } = require("./helper/readWrite");
 
-module.exports = mongoose.model("Expenses", expenseSchema)
+const filepath = path.join(process.cwd(), "backend", "data", "Expenses.json");
+
+class Expenses {
+  constructor({ name, amount, date = new Date().toISOString(), label }) {
+    this._id = `e${new Date().getTime()}${Math.floor(
+      Math.random() * 10 ** 10
+    )}`;
+    this.name = name;
+    this.amount = amount;
+    this.label = label;
+    this.createdAt = new Date(date).toISOString();
+  }
+  async save() {
+    try {
+      const oldExpensesData = await readFile(filepath);
+      oldExpensesData.push(this);
+      const sortedExpensesData = oldExpensesData.sort((a,b)=>{
+        const aDate = new Date(a.createdAt).getTime();
+        const bDate = new Date(b.createdAt).getTime();
+        return +aDate - +bDate;
+      })
+      await writeFile(filepath, sortedExpensesData);
+      return this;
+    } catch (error) {
+      throw new Error("Failed to save the expense");
+    }
+  }
+
+  static async findById(id) {
+    try {
+      const expenses = await readFile(filepath);
+      const expense = expenses.find((exp) => exp._id === "" + id);
+      if (!expense) {
+        return {};
+      }
+      return expense;
+    } catch (error) {
+      throw new Error("Failed to fetch the expense");
+    }
+  }
+
+  static async findByIdAndDelete(id = "") {
+    try {
+      const expenseToDelete = await Expenses.findById(id);
+
+      const oldExpensesData = await readFile(filepath);
+      const updatedExpensesData = oldExpensesData.filter(
+        (expense) => expense._id !== expenseToDelete._id
+      );
+      await writeFile(filepath, updatedExpensesData);
+      return expenseToDelete;
+    } catch (error) {
+      throw new Error("Failed to delete the expense");
+    }
+  }
+
+  static async update(expense) {
+    try {
+      const oldExpensesData = await readFile(filepath);
+      const expenseIndex = oldExpensesData.findIndex(
+        (exp) => exp._id === expense._id
+      );
+      oldExpensesData[expenseIndex] = expense;
+
+      await writeFile(filepath, oldExpensesData);
+      return expense;
+    } catch (error) {
+      throw new Error("Failed to Update the expense");
+    }
+  }
+
+  static async findOne(option = {}) {
+    try {
+      const expenses = await readFile(filepath);
+      for (let i = 0; i < expenses.length; i++) {
+        for (let key in option) {
+          if (expenses[i][key] === option[key]) {
+            return expenses[i];
+          }
+        }
+      }
+      return null;
+    } catch (error) {
+      throw new Error("Failed to fetch the expense");
+    }
+  }
+
+  static async find(option) {
+    try {
+      const expenses = await readFile(filepath);
+      if (!option) {
+        return expenses;
+      }
+      const filteredExpenses = expenses.filter((expense) => {
+        for (let key in option) {
+          return expense[key] === option[key];
+        }
+      });
+      return filteredExpenses;
+    } catch (error) {
+      throw new Error("Failed to fetch all expenses");
+    }
+  }
+
+  static async findManyById(idArr = []) {
+    const allExpenses = await Expenses.find();
+    const populatedExpenses = allExpenses.filter((expense) => {
+      return idArr.some((id) => id === expense._id);
+    });
+    return populatedExpenses;
+  }
+
+  static async deleteOne(option = {}) {
+    try {
+      const expense = await Expenses.findOne(option);
+      if (!expense) {
+        return {};
+      }
+      const deletedExpense = await Expenses.findByIdAndDelete(expense?._id);
+      return deletedExpense;
+    } catch (error) {
+      throw new Error("Failed to delete that expense");
+    }
+  }
+
+  static async deleteMany(option = {}) {
+    try {
+      const expensesToBeDeleted = await Expenses.find(option);
+      const allExpenses = await readFile(filepath);
+      const remainingExpenses = allExpenses.filter(
+        (allExpenseItem) =>
+          !expensesToBeDeleted.some(
+            (expenseItemToBeDeleted) =>
+              expenseItemToBeDeleted._id === allExpenseItem._id
+          )
+      );
+      await writeFile(filepath, remainingExpenses);
+      return expensesToBeDeleted;
+    } catch (error) {
+      throw new Error("Failed to delete expenses");
+    }
+  }
+
+  static async deleteManyById(idArr = []) {
+    const allExpenses = await Expenses.find();
+    const remainingExpenses = allExpenses.filter((expense) => {
+      return !idArr.some((id) => id === expense._id);
+    });
+    await writeFile(filepath, remainingExpenses);
+    return remainingExpenses;
+  }
+}
+
+module.exports = Expenses;

backend/Model/Labels.js
@@ -1,21 +1,166 @@
+const path = require("path");
+
 const Expenses = require("./Expenses");
+const { readFile, writeFile } = require("./helper/readWrite");
 
-const { default: mongoose, Schema } = require("mongoose");
+const filepath = path.join(process.cwd(), "backend", "data", "Labels.json");
 
-const Label = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  budget: {
-    type: String,
-    required: true,
-  },
-  totalExpense: {
-    type: Number,
-    default: 0,
-  },
-  expenses: [{ type: Schema.ObjectId, ref: Expenses }],
-});
+class Labels {
+  constructor({ name, budget }) {
+    this._id = `l${new Date().getTime()}${Math.floor(
+      Math.random() * 10 ** 10
+    )}`;
+    this.name = name;
+    this.budget = budget;
+    this.totalExpense = 0;
+    this.expenses = [];
+  }
+  async save() {
+    try {
+      const oldLabelsData = await readFile(filepath);
+      oldLabelsData.push(this);
 
-module.exports = mongoose.model("Label", Label);
+      await writeFile(filepath, oldLabelsData);
+      return this;
+    } catch (error) {
+      throw new Error("Failed to save the Label");
+    }
+  }
+
+  static async findById(id = "") {
+    try {
+      const labels = await readFile(filepath);
+      const label = labels.find((exp) => exp._id === "" + id);
+      if (!label) {
+        return null;
+      }
+      return label;
+    } catch (error) {
+      throw new Error("Failed to fetch the label");
+    }
+  }
+
+  static async findByIdAndDelete(id = "") {
+    try {
+      const labelToDelete = await Labels.findById(id);
+
+      const oldLabelsData = await readFile(filepath);
+      const updatedLabelsData = oldLabelsData.filter(
+        (label) => label._id !== labelToDelete._id
+      );
+      await writeFile(filepath, updatedLabelsData);
+      return labelToDelete;
+    } catch (error) {
+      throw new Error("Failed to delete the label");
+    }
+  }
+
+  static async update(label) {
+    try {
+      const oldlabelsData = await readFile(filepath);
+      const labelIndex = oldlabelsData.findIndex(
+        (lbl) => lbl._id === label._id
+      );
+      oldlabelsData[labelIndex] = label;
+
+      await writeFile(filepath, oldlabelsData);
+      return label;
+    } catch (error) {
+      throw new Error("Failed to Update the label");
+    }
+  }
+
+  static async findOne(option = {}) {
+    try {
+      const labels = await readFile(filepath);
+      for (let i = 0; i < labels.length; i++) {
+        for (let key in option) {
+          if (labels[i][key] === option[key]) {
+            return labels[i];
+          }
+        }
+      }
+      return null;
+    } catch (error) {
+      throw new Error("Failed to fetch the label");
+    }
+  }
+
+  static async find(option) {
+    try {
+      const labels = await readFile(filepath);
+      if (!option) {
+        return labels;
+      }
+      const filteredLabels = labels.filter((label) => {
+        for (let key in option) {
+          return label[key] === option[key];
+        }
+      });
+      return filteredLabels;
+    } catch (error) {
+      throw new Error("Failed to fetch all labels");
+    }
+  }
+
+  static async deleteOne(option = {}) {
+    try {
+      const label = await Labels.findOne(option);
+      if (!label) {
+        return null;
+      }
+      const deletedLabel = await Labels.findByIdAndDelete(label?._id);
+      return deletedLabel;
+    } catch (error) {
+      throw new Error("Failed to delete that label");
+    }
+  }
+
+  static async deleteMany(option = {}) {
+    try {
+      const labelsToBeDeleted = await Labels.find(option);
+      const allLabels = await readFile(filepath);
+      const remainingLabels = allLabels.filter(
+        (allLabelItem) =>
+          !labelsToBeDeleted.some(
+            (labelItemToBeDeleted) =>
+              labelItemToBeDeleted._id === allLabelItem._id
+          )
+      );
+      await writeFile(filepath, remainingLabels);
+      return labelsToBeDeleted;
+    } catch (error) {
+      throw new Error("Failed to delete expenses");
+    }
+  }
+
+  static async deleteManyById(idArr = []) {
+    const allLabels = await Labels.find();
+    const remainingLabels = allLabels.filter((label) => {
+      return !idArr.some((id) => id === label._id);
+    });
+    await writeFile(filepath, remainingLabels);
+    return remainingLabels;
+  }
+
+  static async findByIdAndPopulate(id = "") {
+    try {
+      const label = await Labels.findById(id);
+      const labelsExpenses = await Expenses.findManyById(label.expenses);
+      label.expenses = labelsExpenses;
+      return label;
+    } catch (error) {
+      throw new Error("Failed to find or populate label");
+    }
+  }
+
+  static async findManyById(idArr = []) {
+    const allLabels = await Labels.find();
+    const populatedLabels = allLabels.filter((label) => {
+      return idArr.some((id) => id === label._id);
+    });
+    return populatedLabels;
+  }
+}
+
+module.exports = Labels;

backend/Model/User.js
@@ -1,25 +1,100 @@
-const { Schema, default: mongoose } = require("mongoose");
-const Label = require("./Labels");
+const path = require("path");
+const fs = require("fs/promises")
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  labels: [{ type: Schema.ObjectId, ref: Label }],
-});
+const Labels = require("./Labels");
+const { readFile, writeFile } = require("./helper/readWrite");
 
-module.exports = mongoose.model("User", UserSchema);
+const filepath = path.join(process.cwd(), "backend", "data", "Users.json");
+
+class User {
+  constructor({ name, email, password }) {
+    this._id = `u${new Date().getTime()}${Math.floor(
+      Math.random() * 10 ** 10
+    )}`;
+    this.name = name;
+    this.password = password;
+    this.email = email;
+    this.labels = [];
+  }
+
+  async save() {
+    const users = await readFile(filepath);
+    users.push(this);
+    await writeFile(filepath, users);
+    return this;
+  }
+
+  static async update(user) {
+    try {
+      const oldUserData = await readFile(filepath);
+      const userIndex = oldUserData.findIndex(
+        (usr) => usr._id === user._id
+      );
+      oldUserData[userIndex] = user;
+
+      await writeFile(filepath, oldUserData);
+      return user;
+    } catch (error) {
+      throw new Error("Failed to Update the label");
+    }
+  }
+
+  static async findById(id = "") {
+    try {
+      const users = await readFile(filepath);
+      const user = users.find((exp) => exp._id === "" + id);
+      if (!user) {
+        return null;
+      }
+      return user;
+    } catch (error) {
+      throw new Error("Failed to fetch the user");
+    }
+  }
+
+  static async findByIdAndDelete(id = "") {
+    try {
+      const userToDelete = await User.findById(id);
+
+      const oldUsersData = await readFile(filepath);
+      const updatedUsersData = oldUsersData.filter(
+        (user) => user._id !== userToDelete._id
+      );
+      await writeFile(filepath, updatedUsersData);
+      return userToDelete;
+    } catch (error) {
+      throw new Error("Failed to delete the user");
+    }
+  }
+
+  static async findOne(option = {}) {
+    try {
+      const filedir = await fs.readdir(`${process.cwd()}/backend/data`)
+      throw new Error(`${filedir.join()}1`)
+      const users = await readFile(filepath);
+      for (let i = 0; i < users.length; i++) {
+        for (let key in option) {
+          if (users[i][key] === option[key]) {
+            return users[i];
+          }
+        }
+      }
+      return null;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async findByIdAndPopulate(id = "") {
+    try {
+      const user = await User.findById(id);
+      const userLabels = await Labels.findManyById(user.labels);
+      user.labels = userLabels;
+      return user;
+    } catch (error) {
+      throw new Error("Failed to find or populate user");
+    }
+  }
+}
+
+module.exports = User;

ShivaBhattarai404/expenses_tracker

backend/Model/Expenses.js
@@ -1,15 +1,158 @@
-const {default: mongoose, Schema} = require("mongoose");
+const path = require("path");
 
-const expenseSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    amount: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-}, {timestamps: true})
+const { readFile, writeFile } = require("./helper/readWrite");
 
-module.exports = mongoose.model("Expenses", expenseSchema)
+const filepath = path.join(process.cwd(), "backend", "tmp", "Expenses.json");
+
+class Expenses {
+  constructor({ name, amount, date = new Date().toISOString(), label }) {
+    this._id = `e${new Date().getTime()}${Math.floor(
+      Math.random() * 10 ** 10
+    )}`;
+    this.name = name;
+    this.amount = amount;
+    this.label = label;
+    this.createdAt = new Date(date).toISOString();
+  }
+  async save() {
+    try {
+      const oldExpensesData = await readFile(filepath);
+      oldExpensesData.push(this);
+      const sortedExpensesData = oldExpensesData.sort((a,b)=>{
+        const aDate = new Date(a.createdAt).getTime();
+        const bDate = new Date(b.createdAt).getTime();
+        return +aDate - +bDate;
+      })
+      await writeFile(filepath, sortedExpensesData);
+      return this;
+    } catch (error) {
+      throw new Error("Failed to save the expense");
+    }
+  }
+
+  static async findById(id) {
+    try {
+      const expenses = await readFile(filepath);
+      const expense = expenses.find((exp) => exp._id === "" + id);
+      if (!expense) {
+        return {};
+      }
+      return expense;
+    } catch (error) {
+      throw new Error("Failed to fetch the expense");
+    }
+  }
+
+  static async findByIdAndDelete(id = "") {
+    try {
+      const expenseToDelete = await Expenses.findById(id);
+
+      const oldExpensesData = await readFile(filepath);
+      const updatedExpensesData = oldExpensesData.filter(
+        (expense) => expense._id !== expenseToDelete._id
+      );
+      await writeFile(filepath, updatedExpensesData);
+      return expenseToDelete;
+    } catch (error) {
+      throw new Error("Failed to delete the expense");
+    }
+  }
+
+  static async update(expense) {
+    try {
+      const oldExpensesData = await readFile(filepath);
+      const expenseIndex = oldExpensesData.findIndex(
+        (exp) => exp._id === expense._id
+      );
+      oldExpensesData[expenseIndex] = expense;
+
+      await writeFile(filepath, oldExpensesData);
+      return expense;
+    } catch (error) {
+      throw new Error("Failed to Update the expense");
+    }
+  }
+
+  static async findOne(option = {}) {
+    try {
+      const expenses = await readFile(filepath);
+      for (let i = 0; i < expenses.length; i++) {
+        for (let key in option) {
+          if (expenses[i][key] === option[key]) {
+            return expenses[i];
+          }
+        }
+      }
+      return null;
+    } catch (error) {
+      throw new Error("Failed to fetch the expense");
+    }
+  }
+
+  static async find(option) {
+    try {
+      const expenses = await readFile(filepath);
+      if (!option) {
+        return expenses;
+      }
+      const filteredExpenses = expenses.filter((expense) => {
+        for (let key in option) {
+          return expense[key] === option[key];
+        }
+      });
+      return filteredExpenses;
+    } catch (error) {
+      throw new Error("Failed to fetch all expenses");
+    }
+  }
+
+  static async findManyById(idArr = []) {
+    const allExpenses = await Expenses.find();
+    const populatedExpenses = allExpenses.filter((expense) => {
+      return idArr.some((id) => id === expense._id);
+    });
+    return populatedExpenses;
+  }
+
+  static async deleteOne(option = {}) {
+    try {
+      const expense = await Expenses.findOne(option);
+      if (!expense) {
+        return {};
+      }
+      const deletedExpense = await Expenses.findByIdAndDelete(expense?._id);
+      return deletedExpense;
+    } catch (error) {
+      throw new Error("Failed to delete that expense");
+    }
+  }
+
+  static async deleteMany(option = {}) {
+    try {
+      const expensesToBeDeleted = await Expenses.find(option);
+      const allExpenses = await readFile(filepath);
+      const remainingExpenses = allExpenses.filter(
+        (allExpenseItem) =>
+          !expensesToBeDeleted.some(
+            (expenseItemToBeDeleted) =>
+              expenseItemToBeDeleted._id === allExpenseItem._id
+          )
+      );
+      await writeFile(filepath, remainingExpenses);
+      return expensesToBeDeleted;
+    } catch (error) {
+      throw new Error("Failed to delete expenses");
+    }
+  }
+
+  static async deleteManyById(idArr = []) {
+    const allExpenses = await Expenses.find();
+    const remainingExpenses = allExpenses.filter((expense) => {
+      return !idArr.some((id) => id === expense._id);
+    });
+    await writeFile(filepath, remainingExpenses);
+    return remainingExpenses;
+  }
+}
+
+module.exports = Expenses;

backend/Model/Labels.js
@@ -1,21 +1,166 @@
+const path = require("path");
+
 const Expenses = require("./Expenses");
+const { readFile, writeFile } = require("./helper/readWrite");
 
-const { default: mongoose, Schema } = require("mongoose");
+const filepath = path.join(process.cwd(), "backend", "tmp", "Labels.json");
 
-const Label = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  budget: {
-    type: String,
-    required: true,
-  },
-  totalExpense: {
-    type: Number,
-    default: 0,
-  },
-  expenses: [{ type: Schema.ObjectId, ref: Expenses }],
-});
+class Labels {
+  constructor({ name, budget }) {
+    this._id = `l${new Date().getTime()}${Math.floor(
+      Math.random() * 10 ** 10
+    )}`;
+    this.name = name;
+    this.budget = budget;
+    this.totalExpense = 0;
+    this.expenses = [];
+  }
+  async save() {
+    try {
+      const oldLabelsData = await readFile(filepath);
+      oldLabelsData.push(this);
 
-module.exports = mongoose.model("Label", Label);
+      await writeFile(filepath, oldLabelsData);
+      return this;
+    } catch (error) {
+      throw new Error("Failed to save the Label");
+    }
+  }
+
+  static async findById(id = "") {
+    try {
+      const labels = await readFile(filepath);
+      const label = labels.find((exp) => exp._id === "" + id);
+      if (!label) {
+        return null;
+      }
+      return label;
+    } catch (error) {
+      throw new Error("Failed to fetch the label");
+    }
+  }
+
+  static async findByIdAndDelete(id = "") {
+    try {
+      const labelToDelete = await Labels.findById(id);
+
+      const oldLabelsData = await readFile(filepath);
+      const updatedLabelsData = oldLabelsData.filter(
+        (label) => label._id !== labelToDelete._id
+      );
+      await writeFile(filepath, updatedLabelsData);
+      return labelToDelete;
+    } catch (error) {
+      throw new Error("Failed to delete the label");
+    }
+  }
+
+  static async update(label) {
+    try {
+      const oldlabelsData = await readFile(filepath);
+      const labelIndex = oldlabelsData.findIndex(
+        (lbl) => lbl._id === label._id
+      );
+      oldlabelsData[labelIndex] = label;
+
+      await writeFile(filepath, oldlabelsData);
+      return label;
+    } catch (error) {
+      throw new Error("Failed to Update the label");
+    }
+  }
+
+  static async findOne(option = {}) {
+    try {
+      const labels = await readFile(filepath);
+      for (let i = 0; i < labels.length; i++) {
+        for (let key in option) {
+          if (labels[i][key] === option[key]) {
+            return labels[i];
+          }
+        }
+      }
+      return null;
+    } catch (error) {
+      throw new Error("Failed to fetch the label");
+    }
+  }
+
+  static async find(option) {
+    try {
+      const labels = await readFile(filepath);
+      if (!option) {
+        return labels;
+      }
+      const filteredLabels = labels.filter((label) => {
+        for (let key in option) {
+          return label[key] === option[key];
+        }
+      });
+      return filteredLabels;
+    } catch (error) {
+      throw new Error("Failed to fetch all labels");
+    }
+  }
+
+  static async deleteOne(option = {}) {
+    try {
+      const label = await Labels.findOne(option);
+      if (!label) {
+        return null;
+      }
+      const deletedLabel = await Labels.findByIdAndDelete(label?._id);
+      return deletedLabel;
+    } catch (error) {
+      throw new Error("Failed to delete that label");
+    }
+  }
+
+  static async deleteMany(option = {}) {
+    try {
+      const labelsToBeDeleted = await Labels.find(option);
+      const allLabels = await readFile(filepath);
+      const remainingLabels = allLabels.filter(
+        (allLabelItem) =>
+          !labelsToBeDeleted.some(
+            (labelItemToBeDeleted) =>
+              labelItemToBeDeleted._id === allLabelItem._id
+          )
+      );
+      await writeFile(filepath, remainingLabels);
+      return labelsToBeDeleted;
+    } catch (error) {
+      throw new Error("Failed to delete expenses");
+    }
+  }
+
+  static async deleteManyById(idArr = []) {
+    const allLabels = await Labels.find();
+    const remainingLabels = allLabels.filter((label) => {
+      return !idArr.some((id) => id === label._id);
+    });
+    await writeFile(filepath, remainingLabels);
+    return remainingLabels;
+  }
+
+  static async findByIdAndPopulate(id = "") {
+    try {
+      const label = await Labels.findById(id);
+      const labelsExpenses = await Expenses.findManyById(label.expenses);
+      label.expenses = labelsExpenses;
+      return label;
+    } catch (error) {
+      throw new Error("Failed to find or populate label");
+    }
+  }
+
+  static async findManyById(idArr = []) {
+    const allLabels = await Labels.find();
+    const populatedLabels = allLabels.filter((label) => {
+      return idArr.some((id) => id === label._id);
+    });
+    return populatedLabels;
+  }
+}
+
+module.exports = Labels;

backend/Model/User.js
@@ -1,25 +1,101 @@
-const { Schema, default: mongoose } = require("mongoose");
-const Label = require("./Labels");
+const path = require("path");
+const fs = require("fs/promises")
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  labels: [{ type: Schema.ObjectId, ref: Label }],
-});
+const Labels = require("./Labels");
+const { readFile, writeFile } = require("./helper/readWrite");
 
-module.exports = mongoose.model("User", UserSchema);
+const filepath = path.join(process.cwd(), "backend", "tmp", "Users.json");
+
+class User {
+  constructor({ name, email, password }) {
+    this._id = `u${new Date().getTime()}${Math.floor(
+      Math.random() * 10 ** 10
+    )}`;
+    this.name = name;
+    this.password = password;
+    this.email = email;
+    this.labels = [];
+  }
+
+  async save() {
+    const users = await readFile(filepath);
+    users.push(this);
+    await writeFile(filepath, users);
+    return this;
+  }
+
+  static async update(user) {
+    try {
+      const oldUserData = await readFile(filepath);
+      const userIndex = oldUserData.findIndex(
+        (usr) => usr._id === user._id
+      );
+      oldUserData[userIndex] = user;
+
+      await writeFile(filepath, oldUserData);
+      return user;
+    } catch (error) {
+      throw new Error("Failed to Update the label");
+    }
+  }
+
+  static async findById(id = "") {
+    try {
+      const users = await readFile(filepath);
+      const user = users.find((exp) => exp._id === "" + id);
+      if (!user) {
+        return null;
+      }
+      return user;
+    } catch (error) {
+      throw new Error("Failed to fetch the user");
+    }
+  }
+
+  static async findByIdAndDelete(id = "") {
+    try {
+      const userToDelete = await User.findById(id);
+
+      const oldUsersData = await readFile(filepath);
+      const updatedUsersData = oldUsersData.filter(
+        (user) => user._id !== userToDelete._id
+      );
+      await writeFile(filepath, updatedUsersData);
+      return userToDelete;
+    } catch (error) {
+      throw new Error("Failed to delete the user");
+    }
+  }
+
+  static async findOne(option = {}) {
+    try {
+      const users = await readFile(filepath);
+      if(users.length === 0){
+        return null;
+      }
+      for (let i = 0; i < users.length; i++) {
+        for (let key in option) {
+          if (users[i][key] === option[key]) {
+            return users[i];
+          }
+        }
+      }
+      return null;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async findByIdAndPopulate(id = "") {
+    try {
+      const user = await User.findById(id);
+      const userLabels = await Labels.findManyById(user.labels);
+      user.labels = userLabels;
+      return user;
+    } catch (error) {
+      throw new Error("Failed to find or populate user");
+    }
+  }
+}
+
+module.exports = User;